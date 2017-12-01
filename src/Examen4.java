55f95c64a05d5502f0d673b986be197e7f5cbf2f
JavierMondragoD <31265510+JavierMondragoD@users.noreply.github.com>
2017-11-30 22:15:09 -0600

    Este es mi trabajo de la materia estructura de datos del instituto tecnologico de cd altamirano del 3er semestre por el alumno Jose Javier Mondragon Delgado apoyado por el profesor L.I. Leonel Gonzales Vidales

---
+++ src/Examen4.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author JOSE JAVIER
+ */
+public class Examen4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        ArbolBinario arbol = new  ArbolBinario ());
+        
+     arbol.agregarNodo (50);
+     arbol.agregarNodo(25);
+     arbol.agregarNodo(75);
+     arbol.agregarNodo(10);
+     arbol.agregarNodo(40);
+     arbol.agregarNodo(60);
+     arbol.agregarNodo(90);
+     arbol.agregarNodo(35);
+     arbol.agregarNodo(45);
+     arbol.agregarNodo(70);
+     arbol.agregarNodo(42);
+      
+     System.out.println(ArbolBinario.AlturaCalcular());
+     System.out.println(contar.arbol);
+     arbol.inorden.sinhacerrecursividad(binario.tenerlaraiz);
+    }
+    
+}
